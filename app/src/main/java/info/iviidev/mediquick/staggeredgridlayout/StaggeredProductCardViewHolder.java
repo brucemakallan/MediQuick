367d904ad9997f38076598e47265bc99854395c7
Bruce Makaaru <brucemakallan@gmail.com>
2018-06-18 21:15:28 +0300

    Install SHRINE Theme

---
+++ app/src/main/java/info/iviidev/mediquick/staggeredgridlayout/StaggeredProductCardViewHolder.java
@@ -0,0 +1,24 @@
+package info.iviidev.mediquick.staggeredgridlayout;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+import info.iviidev.mediquick.R;
+
+public class StaggeredProductCardViewHolder extends RecyclerView.ViewHolder {
+
+    public NetworkImageView productImage;
+    public TextView productTitle;
+    public TextView productPrice;
+
+    StaggeredProductCardViewHolder(@NonNull View itemView) {
+        super(itemView);
+        productImage = itemView.findViewById(R.id.product_image);
+        productTitle = itemView.findViewById(R.id.product_title);
+        productPrice = itemView.findViewById(R.id.product_price);
+    }
+}
